9a71ab51f570c6335853b703a67ef3c5c9785d26
Steve <B00418234@studentmail.uws.ac.uk>
2022-11-22 14:25:28 +0000

    Set up Models

--- myapp/app.js
+++ myapp/app.js
@@ -9,6 +9,18 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://root:example@mongodb:27017';
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -23,12 +35,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
